5928c679dbb31f81824e91765cef1c2fd8f1992c
gak92 <44802922+gak92@users.noreply.github.com>
2022-11-29 01:30:18 +0500

    Update seed file to insert data into the table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(message: "Hello React and Rails")
+Greeting.create(message: "Hi Javascript")
+Greeting.create(message: "Hello Ruby on Rails")
+Greeting.create(message: "Welcome to Final Capstone")
+Greeting.create(message: "Good Morning")
+
