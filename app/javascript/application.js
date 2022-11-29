b3dc7b64a95365acd7d319faef5516f24bf89c4d
gak92 <44802922+gak92@users.noreply.github.com>
2022-11-29 20:40:05 +0500

    Create app component and render it view

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,17 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
+// import "@hotwired/turbo-rails"
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+// function App() {
+//   return (
+//     <h1>Hello React and Rails!</h1>
+//   );
+// }
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
