880057b73bbbc331873a5dafa17f48a8065c0b46
gak92 <44802922+gak92@users.noreply.github.com>
2022-11-30 01:33:36 +0500

    Update application.js to add store and BrowserRouter

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,17 +1,18 @@
 // Entry point for the build script in your package.json
-// import "@hotwired/turbo-rails"
+import "@hotwired/turbo-rails"
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
-
-// function App() {
-//   return (
-//     <h1>Hello React and Rails!</h1>
-//   );
-// }
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'),
 );
