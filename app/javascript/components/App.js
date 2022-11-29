8611355fa66105559b3540aaa433e23ef8f9c21b
gak92 <44802922+gak92@users.noreply.github.com>
2022-11-30 01:34:19 +0500

    Update App.js component to add Greeting Component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,8 +1,15 @@
  import React from "react";
- 
+ import { Routes, Route } from 'react-router-dom';
+ import Greeting from './Greeting';
+
  const App = () => {
   return (
-    <h1>Welcome to React and Rails!</h1>
+    <>
+      <h1>Welcome to React and Rails!</h1>
+      <Routes>
+        <Route exact path='/' element={<Greeting />} />
+      </Routes>
+    </>
   );
 }
 
