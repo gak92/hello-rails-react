3611b0986b9730f7a591e478118874c12e97ce71
gak92 <44802922+gak92@users.noreply.github.com>
2022-11-29 14:18:43 +0500

    Add route for the api greeting

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+      get '/random_greeting', to: 'greetings#greeting'
+    end
+  end
+
   # get 'greetings/index'
   resources :greeting
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
